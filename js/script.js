253cfc1d5cd2ef0c8ececbee90fee7598035ec26
kkarakhanian <karakhanian.k@gmail.com>
2025-06-29 23:06:02 +0300

    create new branch with hw-31

--- js/script.js
+++ js/script.js
@@ -1,71 +1,33 @@
 'use strict';
 
-let users = [
-    {
-        "index": 0,
-        "isActive": true,
-        "balance": "$2,226.60",
-        "name": "Eugenia Sawyer",
-        "gender": "female",
-        "phone": "+1 (840) 583-3207",
-        "address": "949 John Street, Rose, Puerto Rico, 1857"
-    },
-    {
-        "index": 1,
-        "isActive": true,
-        "balance": "$2,613.77",
-        "name": "Pauline Gallegos",
-        "gender": "female",
-        "phone": "+1 (985) 593-3328",
-        "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
-    },
-    {
-        "index": 2,
-        "isActive": false,
-        "balance": "$3,976.41",
-        "name": "Middleton Chaney",
-        "gender": "male",
-        "phone": "+1 (995) 591-2478",
-        "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-    },
-    {
-        "index": 3,
-        "isActive": true,
-        "balance": "$1,934.58",
-        "name": "Burns Poole",
-        "gender": "male",
-        "phone": "+1 (885) 559-3422",
-        "address": "730 Seba Avenue, Osage, Alabama, 6290"
-    },
-    {
-        "index": 4,
-        "isActive": true,
-        "balance": "$3,261.65",
-        "name": "Mcfadden Horne",
-        "gender": "male",
-        "phone": "+1 (942) 565-3988",
-        "address": "120 Scholes Street, Kirk, Michigan, 1018"
-    },
-    {
-        "index": 5,
-        "isActive": false,
-        "balance": "$1,790.56",
-        "name": "Suzette Lewis",
-        "gender": "female",
-        "phone": "+1 (837) 586-3283",
-        "address": "314 Dunne Place, Bawcomville, Guam, 9053"
-    }
-]
-const phones = [];
-let total = 0;
-
-for (let i = 0; i < users.length; i++) {
-    const balance = parseFloat(users[i].balance.replace('$', '').replace(',', ''));
-
-    total += balance;
-    if (balance > 2000) {
-        phones.push(users[i].phone);
-    }
+//apply
+Function.prototype.myApply = function(context, args) {
+    context = context || window;
+    context.temp = this;
+    const result = context.temp(args[0], args[1], args[2]);
+    delete context.temp;
+    return result;
 }
-console.log("Telephones with balance $2000+:", phones);
-console.log("Sum of all balances:", total.toFixed(2));
+function sayHi(a, b) {
+    console.log(this.name, a, b);
+}
+let user = { name: "Lena" };
+sayHi.myApply(user, [1, 2]);
+
+//bind
+Function.prototype.myBind = function(context) {
+    const self = this;
+    return function(a, b) {
+        context.temp = self;
+        const result = context.temp(a, b);
+        delete context.temp;
+        return result;
+    };
+}
+function greet(a, b) {
+    console.log(this.name, a, b);
+}
+
+const user2 = { name: "Max" };
+const bound = greet.myBind(user2);
+bound("Hi", "!");
