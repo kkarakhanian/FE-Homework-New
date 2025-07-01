2ff7b8510116dd899c08412bf6f62c6e2be929ca
kkarakhanian <karakhanian.k@gmail.com>
2025-07-01 12:55:58 +0300

    create new branch with hw-32

--- js/script.js
+++ js/script.js
@@ -1,33 +1,46 @@
 'use strict';
 
-//apply
-Function.prototype.myApply = function(context, args) {
-    context = context || window;
-    context.temp = this;
-    const result = context.temp(args[0], args[1], args[2]);
-    delete context.temp;
+
+const findValuesByKey = (obj, targetKey) => {
+    const result =[];
+
+    function search(currentObj) {
+        if (currentObj === null || typeof currentObj !== 'object') {
+            return;
+        }
+        if (!Array.isArray(currentObj)) {
+            for (const key in currentObj) {
+                if (Object.prototype.hasOwnProperty.call(currentObj, key)) {
+                    if (key === targetKey) {
+                        result.push(currentObj[key]);
+                    }
+                    search(currentObj[key]);
+                }
+            }
+        } else {
+            for (const item of currentObj) {
+                search(item);
+            }
+        }
+    }
+    search(obj);
     return result;
 }
-function sayHi(a, b) {
-    console.log(this.name, a, b);
-}
-let user = { name: "Lena" };
-sayHi.myApply(user, [1, 2]);
 
-//bind
-Function.prototype.myBind = function(context) {
-    const self = this;
-    return function(a, b) {
-        context.temp = self;
-        const result = context.temp(a, b);
-        delete context.temp;
-        return result;
-    };
-}
-function greet(a, b) {
-    console.log(this.name, a, b);
-}
+const data = {
+    id: 1,
+    name: "root",
+    meta: {
+        id: 2,
+        parent: {
+            id: 3,
+            name: "leaf",
+        },
+    },
+    array: [
+        { id: 4 },
+        { name: "node", children: [{ id: 5 }] },
+    ],
+};
 
-const user2 = { name: "Max" };
-const bound = greet.myBind(user2);
-bound("Hi", "!");
+console.log(findValuesByKey(data, "id"));
