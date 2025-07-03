4f88861e75ab54b7a9af21f0bf044fc21d02bbf5
kkarakhanian <karakhanian.k@gmail.com>
2025-07-03 13:55:38 +0300

    create new branch with hw-33

--- js/script.js
+++ js/script.js
@@ -1,46 +1,8 @@
 'use strict';
 
 
-const findValuesByKey = (obj, targetKey) => {
-    const result =[];
-
-    function search(currentObj) {
-        if (currentObj === null || typeof currentObj !== 'object') {
-            return;
-        }
-        if (!Array.isArray(currentObj)) {
-            for (const key in currentObj) {
-                if (Object.prototype.hasOwnProperty.call(currentObj, key)) {
-                    if (key === targetKey) {
-                        result.push(currentObj[key]);
-                    }
-                    search(currentObj[key]);
-                }
-            }
-        } else {
-            for (const item of currentObj) {
-                search(item);
-            }
-        }
-    }
-    search(obj);
-    return result;
-}
-
-const data = {
-    id: 1,
-    name: "root",
-    meta: {
-        id: 2,
-        parent: {
-            id: 3,
-            name: "leaf",
-        },
-    },
-    array: [
-        { id: 4 },
-        { name: "node", children: [{ id: 5 }] },
-    ],
-};
-
-console.log(findValuesByKey(data, "id"));
+const buttonId = document.getElementById('toggle-theme');
+buttonId.addEventListener("click", function () {
+    const bodyElement = document.body;
+    bodyElement.classList.toggle('dark-theme');
+});
